d2bce23786c897bbd058c29eac77aacac8ccbe90
Phantomcat20 <63794264+assudani66@users.noreply.github.com>
2021-09-29 16:38:45 +0530

    Fixed Logic

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var billAmount = document.querySelector("#bill-amount");
-var cashgiven = document.querySelector("#cash-amount");
+const billAmount = document.querySelector("#bill-amount");
+const cashgiven = document.querySelector("#cash-amount");
 const checkbutton = document.querySelector("#btn-calculate");
-var errorMessage = document.querySelector("#error-message");
-var NoofNotes = document.querySelectorAll(".no-of-notes")
+const errorMessage = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes")
 availablenotes = [2000,500,100,50,20,5,1]
 
 
@@ -11,34 +11,33 @@ checkbutton.addEventListener("click" , function ValidatebillAmountandcashamount(
 
     errorMessage.style.display = "none";
     if(billAmount.value > 0 ){
-        if(cashgiven.value >= billAmount.value) {
 
-            var amounttobereturned = cashgiven.value-billAmount.value;
-            console.log(amounttobereturned)
-            calculatechange(amounttobereturned)
+        if(Number(cashgiven.value) >= Number(billAmount.value)) {
+            const amountToBeReturned = cashgiven.value - billAmount.value;
 
-        }else{
-            showmessage("ask for more cash")
+            calculateChange(amountToBeReturned);
+
+        } else {
+            showMessage("Ask for more cash");
+
         }
 
     }else{
-        showmessage("bill amount invalid")
+        
+        showMessage("Bill Amount is Invalid");
     }
-
-})
-function showmessage(message){
+});
+function showMessage(message){
     errorMessage.style.display = "block";
     errorMessage.innerText = message;
 }
-console.log(availablenotes.length+1)
-function calculatechange(amounttobereturned){
+
+function calculateChange(amounttobereturned){
     for(i=0 ; i<=availablenotes.length +1 ; i++){
        var numberofnotes = Math.trunc(
             amounttobereturned/availablenotes[i]);
         amounttobereturned %= availablenotes[i]
-        NoofNotes[i].innerText = numberofnotes;
-        
-
+        noOfNotes[i].innerText = numberofnotes;
     }
         
 }
